9264f5ce9d7e6f8d2442165966afc2e5a17d5bc7
Clinton Lau <clintonhlau@gmail.com>
2025-08-26 19:30:34 +1000

    successfully connected .env to spotify_api.py

--- scripts/spotify_api.py
+++ scripts/spotify_api.py
@@ -1,1 +1,11 @@
-import spotipy
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(script_dir)
+env_path = os.path.join(project_root, ".env")
+load_dotenv(env_path)
+
+print(os.getenv("SPOTIFY_CLIENT_ID"))
+print(os.getenv("SPOTIFY_CLIENT_SECRET"))
