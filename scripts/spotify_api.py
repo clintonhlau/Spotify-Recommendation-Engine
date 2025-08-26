da719fadfa62e2ad2f19f46d95c3a1186e84a581
Clinton Lau <clintonhlau@gmail.com>
2025-08-26 23:45:14 +1000

    added get_token() and get_auth_header(token)

--- scripts/spotify_api.py
+++ scripts/spotify_api.py
@@ -1,11 +1,36 @@
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+import base64
+from requests import post
+import json
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.dirname(script_dir)
 env_path = os.path.join(project_root, ".env")
 load_dotenv(env_path)
 
-print(os.getenv("SPOTIFY_CLIENT_ID"))
-print(os.getenv("SPOTIFY_CLIENT_SECRET"))
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Tyype": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+token = get_token()
+print(token)
